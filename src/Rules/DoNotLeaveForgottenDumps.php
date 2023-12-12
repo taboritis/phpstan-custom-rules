6143f52005f1b80e3f54e7cca14cab757a6a2fab
Piotr Skrobol <pio.skro@gmail.com>
2023-12-12 21:43:17 +0100

    Fix phpStan errors

--- src/Rules/DoNotLeaveForgottenDumps.php
+++ src/Rules/DoNotLeaveForgottenDumps.php
@@ -5,20 +5,31 @@ declare(strict_types=1);
 namespace Taboritis\PhpstanCustomRules\Rules;
 
 use PhpParser\Node;
+use PhpParser\Node\Expr\FuncCall;
 use PHPStan\Analyser\Scope;
 use PHPStan\Rules\Rule;
 use PHPStan\Rules\RuleErrorBuilder;
 
+/**
+ * @implements Rule<FuncCall>
+ */
 class DoNotLeaveForgottenDumps implements Rule
 {
     public function getNodeType(): string
     {
-        return Node\Expr\FuncCall::class;
+        return FuncCall::class;
     }
+
 
     public function processNode(Node $node, Scope $scope): array
     {
-        $functionName = $node->name->toString();
+        $name = $node->name;
+
+        if (!$name instanceof Node\Name) {
+            return [];
+        }
+
+        $functionName = $name->toLowerString();
 
         return [
             RuleErrorBuilder::message("Do not use {$functionName} function")->build()
